00ebf644042670e7421c1ed1b5144196f386ce1e
Yashashwini Dixit <yashashwinid@valuefy.com>
2022-06-20 00:15:46 +0530

    initialised dahboard pages

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,5 +1,31 @@
 import React from "react";
-
+import { Card, Button } from "react-bootstrap";
+import "./Dashboard.css";
 export default function AdminDashboard() {
-  return <div>AdminDashboard</div>;
+  return (
+    <div>
+      <div className="container">
+        <div className="Cards">
+          <Card>
+            <Card.Body className="carditem">
+              <Card.Title>Flipkart</Card.Title>
+              <Button variant="primary">Subscribe</Button>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body className="carditem">
+              <Card.Title>Flipkart</Card.Title>
+              <Button variant="primary">Subscribe</Button>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body className="carditem">
+              <Card.Title>Flipkart</Card.Title>
+              <Button variant="primary">Subscribe</Button>
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,18 @@
 import React from "react";
-
+import { NavLink } from "react-router-dom";
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <h1>Login</h1>
+      <label for="email">Enter your email:</label>
+      <input type="email" id="email" name="email" />
+      <br />
+      <label for="otp">Enter the OTP received on Email:</label>
+      <input type="number" name="otp" />
+      <br />
+      <NavLink to="/Admin">
+        <button>Submit</button>
+      </NavLink>
+    </div>
+  );
 }
